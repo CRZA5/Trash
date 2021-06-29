017cb129758c44604df43ea72725c751584c36c1
Amal Zacharia <contact@crza.dev>
2021-06-29 21:23:45 +0530

    Update main.cpp

--- 1/main.cpp
+++ 1/main.cpp
@@ -5,12 +5,8 @@ int main()
     int a, b, c;
     cout << "Program to find largest three digit number\n";
     cout << "Enter Three Digits\n";
-    cin >> a >> b >> c;
-    if (a > b && a > c)
-        cout << a << " is big";
-    else if (b > c)
-        cout << b << " is big";
-    else
-        cout << c << " is big";
+    cin >> a >> b >> c;    
+    int largest = a >= b ? a >= c ? a : c : b;
+    cout << largest << " is the largest number";
     return 0;
 }
